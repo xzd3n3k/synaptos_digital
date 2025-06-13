461a7609fe42c85643ee86c05e8c603df0289caf
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-06-13 22:45:49 +0200

    feat(home-screen): add home screen component

---
+++ src/app/screens/home-screen/home-screen.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'syd-home-screen',
+  imports: [],
+  templateUrl: './home-screen.html',
+  styleUrl: './home-screen.scss'
+})
+export class HomeScreen {
+
+}
