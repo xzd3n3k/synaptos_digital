b2b82fccc30bafc3b06b58d756705d10795db472
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-06-13 21:03:20 +0200

    feat(sass): add and implement sass

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,7 +5,7 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.scss'
 })
 export class App {
   protected title = 'synaptosDigital';
