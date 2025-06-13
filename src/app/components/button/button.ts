b089a2bdf30af081593e477e60c4121107c92e10
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-06-13 22:44:17 +0200

    feat(button): add button component

---
+++ src/app/components/button/button.ts
@@ -0,0 +1,23 @@
+import {Component, ElementRef, inject, input} from '@angular/core';
+import {ButtonVariant} from '../../shared/models/button-variant.type';
+import {ButtonSize} from '../../shared/models/button-size.type';
+
+@Component({
+  selector: 'syd-button',
+  imports: [],
+  templateUrl: './button.html',
+  styleUrl: './button.scss'
+})
+export class Button {
+  readonly variant = input<ButtonVariant>('primary');
+  readonly size = input<ButtonSize>('normal');
+  readonly disabled = input<boolean>(false);
+
+  readonly element: HTMLElement;
+
+  private readonly hostElementRef = inject(ElementRef);
+
+  constructor() {
+    this.element = this.hostElementRef.nativeElement;
+  }
+}
