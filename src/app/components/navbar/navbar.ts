b4f7fd06e2649209a56c68d373314993c1c26d37
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-06-13 22:45:24 +0200

    feat(navbar): add navbar component

---
+++ src/app/components/navbar/navbar.ts
@@ -0,0 +1,37 @@
+import {Component, ElementRef, signal, viewChild, WritableSignal} from '@angular/core';
+import { isMobile, isSmallDevice } from '../../shared/utils/device.util';
+import {NavBarLink} from '../../shared/models/nav-bar-link.model';
+import {RouterLink} from '@angular/router';
+import {TranslatePipe} from '@ngx-translate/core';
+import {Button} from '../button/button';
+import {Icon} from '../icon/icon';
+
+@Component({
+  selector: 'syd-navbar',
+  imports: [
+    RouterLink,
+    TranslatePipe,
+    Button,
+    Icon
+  ],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss'
+})
+export class Navbar {
+  private readonly navRef = viewChild.required<ElementRef>('navRef');
+
+  protected readonly isMobile = isMobile;
+  protected readonly isSmallDevice = isSmallDevice;
+
+  protected readonly navLinks: WritableSignal<NavBarLink[]> = signal([
+    { label: 'i18n.navbar.home', route: '/home', clickFunction: () => this.toggleNavbar() },
+    { label: 'FAQ', route: '/faq', clickFunction: () => this.toggleNavbar(), disabled: true },
+    { label: 'i18n.navbar.about', route: '/about', clickFunction: () => this.toggleNavbar(), disabled: true },
+  ]);
+
+  protected toggleNavbar() {
+    if (this.isSmallDevice()) {
+      this.navRef().nativeElement.classList.toggle('responsive_nav');
+    }
+  }
+}
