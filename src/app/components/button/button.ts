4709c7ccea8c7ff7c803c0a301b9a66d88a357f5
Zdeněk Němec <xzdeneknemec@outlook.cz>
2025-06-14 20:36:58 +0200

    feat(button): add shapes to button

--- src/app/components/button/button.ts
+++ src/app/components/button/button.ts
@@ -1,6 +1,7 @@
 import {Component, ElementRef, inject, input} from '@angular/core';
 import {ButtonVariant} from '../../shared/models/button-variant.type';
 import {ButtonSize} from '../../shared/models/button-size.type';
+import {ButtonShape} from '../../shared/models/button-shape.type';
 
 @Component({
   selector: 'syd-button',
@@ -12,6 +13,7 @@ export class Button {
   readonly variant = input<ButtonVariant>('primary');
   readonly size = input<ButtonSize>('normal');
   readonly disabled = input<boolean>(false);
+  readonly shape = input<ButtonShape>('rounded')
 
   readonly element: HTMLElement;
 
